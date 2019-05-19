93145147bfec9128f893fdd2363bede42a21c3da
Santiago <santiagoiamarino@gmail.com>
2019-05-18 22:18:24 -0300

    estado de las puertas de comisaria

--- resources/CustomScripts/commands-server.lua
+++ resources/CustomScripts/commands-server.lua
@@ -3,24 +3,37 @@ local Proxy = module("vrp", "lib/Proxy")
 
 vRP = Proxy.getInterface("vRP")
 
--- AddEventHandler('chatMessage', function(source, name, msg)
---     if msg:sub(1,4) == "/ooc" then
---         CancelEvent()
---         TriggerClientEvent('chatMessage', -1, "OOC | " .. name, { 128, 128, 128 }, string.sub(msg,5))
---     elseif msg:sub(1,3) == "/me" or msg:sub(1,3) == "/do" then
---         CancelEvent()
--- 		TriggerClientEvent('chatMessage', -1, "ME | " .. name, { 255, 0, 0 }, msg:sub(4))
---     elseif msg:sub(1,4) == "/twt" then
---         CancelEvent()
---         local user_id = vRP.getUserId({source})
---         if user_id ~= nil then
---             vRP.getUserIdentity({user_id, function(identity)
---                 if identity ~= nil then
---                     TriggerClientEvent('chatMessage', -1, "^4TWITTER | ^5@"..identity.firstname.. " "..identity.name, { 10, 205, 245 }, msg:sub(5))
---                 end
---             end})
---         end
---     end
--- end)
+AddEventHandler('chatMessage', function(source, name, msg)
+    if msg:sub(1,4) == "/ooc" then
+        CancelEvent()
+        TriggerClientEvent('chatMessage', -1, "OOC | " .. name, { 128, 128, 128 }, string.sub(msg,5))
+    elseif msg:sub(1,3) == "/me" or msg:sub(1,3) == "/do" then
+        CancelEvent()
+        local user_id = vRP.getUserId({source})
+        if user_id ~= nil then
+            vRP.getUserIdentity({user_id, function(identity)
+                if identity ~= nil then
+                    TriggerClientEvent('chatMessage', -1, "ME | " ..identity.firstname.. " "..identity.name, { 255, 0, 0 }, msg:sub(4))
+                end
+            end})
+        end
+    elseif msg:sub(1,4) == "/twt" then
+        CancelEvent()
+        local user_id = vRP.getUserId({source})
+        if user_id ~= nil then
+            vRP.getUserIdentity({user_id, function(identity)
+                if identity ~= nil then
+                    TriggerClientEvent('chatMessage', -1, "^4TWITTER | ^5@"..identity.firstname.. " "..identity.name, { 10, 205, 245 }, msg:sub(5))
+                end
+            end})
+        end
+    end
+end)
+
+RegisterNetEvent('message:inDistanceZone')
+AddEventHandler('message:inDistanceZone', function(target, message)
+    local _source = source
+	TriggerClientEvent('chatMessage', target, "ME | " .. GetPlayerName(_source), {255, 0, 0}, message)
+end)
 
          
--- resources/doors/client.lua
+++ resources/doors/client.lua
@@ -10,6 +10,74 @@ AddEventHandler('vrpdoorsystem:load', function(list)
   doors = list
 end)
 
+Citizen.CreateThread(function()
+  Wait(5000)
+  TriggerServerEvent("vrpdoorsystem:checkGroup", id)
+  while true do
+      Citizen.Wait(0)
+      for _, door in pairs(doors) do
+        local near, dist = isNearJail(door)
+        if(near) then
+          DrawMarker(1, door.x, door.y, door.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 4.0, 1.0,     90, 255, 90, 0      , 0, 0, 1, 0, 0, 0, 0)
+          if(dist<7) then
+              local x = door.x
+              local y = door.y
+              local z = door.z
+              if(door.locked == false) then
+                DrawText3D(x,y,z+0.8, "~g~ Puerta Abierta")
+                DrawText3D(x,y,z+0.7, "~w~ [E] para cerrar")
+              else
+                DrawText3D(x,y,z+0.8, "~r~ Puerta Cerrada")
+                DrawText3D(x,y,z+0.7, "~w~ [E] para abir")
+              end
+              
+          end
+        end
+      end
+  end
+end)
+
+
+function isNearJail(items)
+  local ped = GetPlayerPed(-1)
+  local plyCoords = GetEntityCoords(GetPlayerPed(-1), 0)
+
+  if(GetDistanceBetweenCoords(items.x, items.y, items.z, plyCoords["x"], plyCoords["y"], plyCoords["z"], true) < 20) then
+      return true, GetDistanceBetweenCoords(items.x, items.y, items.z, plyCoords["x"], plyCoords["y"], plyCoords["z"], true)
+  end
+
+  return false
+end
+
+function DrawText3D(x,y,z, text) 
+
+
+
+  local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+  local px,py,pz=table.unpack(GetGameplayCamCoords())
+  local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+
+  local scale = (1/dist)*1.3
+  local fov = (1/GetGameplayCamFov())*50
+  local scale = scale*fov
+ 
+  if onScreen then
+      SetTextScale(0.0*scale, 1.1*scale)
+      SetTextFont(0)
+      SetTextProportional(1)
+      -- SetTextScale(0.0, 0.55)
+      SetTextColour(255, 255, 255, 255)
+      SetTextDropshadow(0, 0, 0, 0, 255)
+      SetTextEdge(2, 0, 0, 0, 150)
+      SetTextDropShadow()
+      SetTextOutline()
+      SetTextEntry("STRING")
+      SetTextCentre(1)
+      AddTextComponentString(text)
+      DrawText(_x,_y)
+  end
+end
+
 
 RegisterNetEvent('vrpdoorsystem:statusSend')
 AddEventHandler('vrpdoorsystem:statusSend', function(i, status)
